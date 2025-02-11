925acab4dd3671948374fca99ea0d41ae4cddee4
Marlen <zomadujsonov@gmail.com>
2025-02-11 17:18:11 +0600

    cars

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TodoModule } from './todo/todo.module';
-import { CarModule } from './car/car.module';
 
 @Module({
-  imports: [TodoModule, CarModule],
+  imports: [TodoModule],
   controllers: [],
   providers: [],
 })
